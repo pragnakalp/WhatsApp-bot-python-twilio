77c73c9c29810f28e55a2ffcba49da10e4cf1f4c
Bimal Patel <pragnakalp.dev10@gmail.com>
2022-04-19 18:30:06 +0530

    Initial Code Push

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# WhatsApp-bot-python-twilio
-Create WhatsApp Bot with Twilio Using Python Tutorial with Examples
+# WhatsApp Bot Using Python Tutorial with Examples
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+we will learn how to connect our whatsapp account with Twilio sandbox and further how we can send different responses through twilio. Moreover, when we send any file to the twilio Whatsapp account then get that file at the server.  
+ 
+## Features
+
+- Text Response
+- Image Response
+- Video Response
+- Document Type Response
+- Audio Response
+
+## Installation
+
+Requires [Python](https://www.python.org) to run.
+
+Create Virtual Evironment.
+
+```sh
+virtualenv env --python=python3
+```
+
+Activate Virtual Evironment.
+
+```sh
+source env/bin/activate
+```
+
+Install All Required Libraries.
+
+```sh
+pip install -r requirements.txt
+```
+Then Run Python flask App.
+
+```sh
+python whatsapp.py
+```
---
+++ whatsapp.py
@@ -0,0 +1,63 @@
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/wa")
+def wa_hello():
+    return "Hello, World!"
+
+@app.route("/wasms", methods=['POST'])
+def wa_sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    Fetch_msg= request.form
+    print("Fetch_msg-->",Fetch_msg)
+
+    try: # Storing the file that user send to the Twilio whatsapp number in our computer
+        msg_url=request.form.get('MediaUrl0')  # Getting the URL of the file
+        print("msg_url-->",msg_url)
+        msg_ext=request.form.get('MediaContentType0')  # Getting the extension for the file
+        print("msg_ext-->",msg_ext)
+        ext = msg_ext.split('/')[-1]
+        print("ext-->",ext)
+        if msg_url != None:
+            json_path = requests.get(msg_url)
+            filename = msg_url.split('/')[-1]
+            open(filename+"."+ext, 'wb').write(json_path.content)  # Storing the file
+    except:
+        print("no url-->>")
+    msg = request.form.get('Body').lower()  # Reading the message from the whatsapp
+    print("msg-->",msg)
+    resp = MessagingResponse()
+    reply=resp.message()
+    # Create reply
+
+    # Text response
+    if msg == "hi":
+       reply.body("hello!")
+
+    # Image response
+    elif msg == "image":
+       reply.media('https://raw.githubusercontent.com/fbsamples/original-coast-clothing/main/public/styles/male-work.jpg',caption="jj ccp")
+    
+    # Audio response
+    elif msg == "audio":
+       reply.media('http://www.largesound.com/ashborytour/sound/brobob.mp3')
+        
+    # Video response
+    elif msg == "video":
+       reply.media('https://www.appsloveworld.com/wp-content/uploads/2018/10/640.mp4')
+    
+    # Document response
+    elif msg == "file":
+       reply.media('http://www.africau.edu/images/default/sample.pdf')
+    
+    else:
+        reply.body("from you")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
